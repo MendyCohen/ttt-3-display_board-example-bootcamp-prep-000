3f4ac654990fe82f5282350dd6b38067062df25c
Mendy <mendybeltcohen@gmail.com>
2018-05-18 17:50:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
   puts "A Tic Tac Toe Board"
   puts "   ""|""   ""|""   "
   puts "-----------"
-  #puts "   ""|""   ""|""   "   
+  puts "   ""|""   ""|""   "   
   #puts "-----------"
   #puts "   ""|""   " "|""   "
  end                
